bc22785d4b06ed303849c463e8de37a96959c1c4
Michael Hahn <mhahn@eventbrite.com>
2013-09-24 03:07:28 -0700

    Add anchors to mysql::client

--- manifests/client.pp
+++ manifests/client.pp
@@ -5,6 +5,13 @@ class mysql::client (
 
   include '::mysql::client::install'
 
+  anchor { 'mysql::client::begin': }
+  anchor { 'mysql::client::end': }
+
+  Anchor['mysql::client::begin'] ->
+  Class['mysql::client::install'] ->
+  Anchor['mysql::client::end']
+
   if $bindings_enable {
     class { 'mysql::bindings':
       java_enable   => true,
